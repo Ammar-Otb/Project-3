a0b6bb5d3aeba5b4d9ced43cb44dae1b75a7b975
Ammar Otaibi <76530937+Ammar-to@users.noreply.github.com>
2022-06-05 11:48:43 +0300

    Add files via upload

---
+++ demodd/src/main/java/com/example/demodd/book/Book.java
@@ -0,0 +1,29 @@
+package com.example.demodd.book;
+
+import com.example.demodd.loan.Loan;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+@AllArgsConstructor @NoArgsConstructor @Setter @Getter
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    private String name;
+    @NotNull
+    private String genre;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    private Set<Loan> loans;
+
+}
---
+++ demodd/src/main/java/com/example/demodd/loan/Loan.java
@@ -0,0 +1,28 @@
+package com.example.demodd.loan;
+
+import com.example.demodd.book.Book;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@AllArgsConstructor @NoArgsConstructor @Setter @Getter
+@Entity
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer userId;
+    private Integer bookId;
+    @ManyToMany(mappedBy = "loans",cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Set<Book> books;
+
+    public Loan(Object o, Integer id, Integer id1) {
+    }
+}
